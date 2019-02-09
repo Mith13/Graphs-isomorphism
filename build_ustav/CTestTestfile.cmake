7c2633242701d7c2c2438956bc3cfcaa8fde7b22
Mithik <jakub.lang@gmail.com>
2019-02-09 10:29:10 +0100

    Clean up

--- build_ustav/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mith/Dropbox/zdrojaky/graphs
-# Build directory: /home/mith/Dropbox/zdrojaky/graphs/build_ustav
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
