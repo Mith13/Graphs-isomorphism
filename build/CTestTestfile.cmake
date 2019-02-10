e0a4b90c87bf130262219c55d13cbf956a0eb5df
Mithik <jakub.lang@gmail.com>
2019-02-10 19:13:08 +0100

    removed visual studio files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mithik/Dropbox/zdrojaky/Graphs
-# Build directory: /home/mithik/Dropbox/zdrojaky/Graphs/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
